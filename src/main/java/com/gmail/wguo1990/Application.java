e940313e30bd6afd3c3eb8097c5cd7428f210039
enesismailoglu <enes.ismailoglu52@gmail.com>
2018-03-02 22:21:56 +0300

    second commit
    second commit

--- src/main/java/com/gmail/wguo1990/Application.java
+++ src/main/java/com/gmail/wguo1990/Application.java
@@ -18,7 +18,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+//app context
 @SpringBootApplication
 @Configuration
 @EnableSwagger2
